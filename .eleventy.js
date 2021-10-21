bebc03b7411fddae9bef25be2b4a68b60e23b76b
ishanbagchi <ishanbagchi4@gmail.com>
2021-10-21 15:40:05 +0530

    made first draft

---
+++ .eleventy.js
@@ -0,0 +1,18 @@
+module.exports = function (eleventyConfig) {
+	// Add filter
+	eleventyConfig.addFilter('dateDisplay', require('./src/_filters/dates.js'));
+
+	eleventyConfig.addWatchTarget('./src/css/');
+	eleventyConfig.addWatchTarget('./src/images/');
+	eleventyConfig.addPassthroughCopy('./src/css/');
+	eleventyConfig.addPassthroughCopy('./src/images/');
+
+	return {
+		dir: {
+			input: 'src',
+			output: 'public',
+		},
+		markdownTemplateEngine: 'njk',
+		pathPrefix: '/eleventy/',
+	};
+};
---
+++ src/posts/post-2.md
@@ -0,0 +1,11 @@
+---
+title: Build my own Bragdoc using Eleventy
+date: 2021-09-19
+categories:
+  - Learning
+  - Eleventy
+public: True
+icon: 🎈
+---
+
+I learned how to use Eleventy to build my own bragdoc!
